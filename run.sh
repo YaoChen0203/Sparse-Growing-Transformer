fb3ea63c43945ab03ac0d71cda86cb49a6442c05
chenyao <116262637+YaoChen0203@users.noreply.github.com>
2026-04-15 00:23:49 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -m torch.distributed.run --nproc_per_node=8 --master_port=29605 scripts/train.py configs/OLMo-300M-Dynamic_Loop-HighEntropyHead-TopKLayers_3-LoopDepth_1-NumHead_2-EmaDecay_0.5_DecayWindow_250-FirstSelectionStep_250.yaml
